c26d8b148a0dc74d9ee40e96b34ddb8488c27c78
ViktoriaMelnyk <oliviatvist12@gmail.com>
2020-10-07 14:42:00 +0200

    Zadanie 4

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
 {
     class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name)
+        public StandardGradeBook(string name) : base (name)
         {
             Type = GradeBookType.Standard;
         }
